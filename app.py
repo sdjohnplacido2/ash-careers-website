3d200763c86213b482409c3e8e6f91427a778bef
Ash <sdjohnplacido2@gmail.com>
2025-12-21 10:40:36 +0800

    Add basic style

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>I love Reg!</h1>"
+    return  render_template('home.html')  #"<h1>I love Reg!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
