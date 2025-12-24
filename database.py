ce22cb1b683d424874b10a1d032a0c746a796373
Ash <sdjohnplacido2@gmail.com>
2025-12-24 16:22:39 +0800

    add application submitted

--- database.py
+++ database.py
@@ -6,6 +6,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
+
 engine = create_engine(DATABASE_URL)
 
 def load_jobs_from_db():
@@ -28,3 +31,27 @@ def load_job_from_db(id):
             return None
         else:
             return dict(row)
+
+
+def add_application_to_db(job_id, data):
+    query = text("""
+        INSERT INTO applications (
+            job_id, full_name, email, linkedin_url,
+            education, work_experience, resume_url
+        )
+        VALUES (
+            :job_id, :full_name, :email, :linkedin_url,
+            :education, :work_experience, :resume_url
+        )
+    """)
+
+    with engine.begin() as conn:  # auto-commit
+        conn.execute(query, {
+            "job_id": job_id,
+            "full_name": data["full_name"],
+            "email": data["email"],
+            "linkedin_url": data.get("linkedin_url"),
+            "education": data.get("education"),
+            "work_experience": data.get("work_experience"),
+            "resume_url": data.get("resume_url")
+        })
